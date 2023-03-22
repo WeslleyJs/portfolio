d0bdcfb2c973bc31fe9045fff87f16445f13cb6c
Weslley Vieira <84158105+TLT-sptb@users.noreply.github.com>
2023-03-22 14:31:45 -0300

    Update Greeting.vue

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -75,17 +75,9 @@ export default {
   filter: brightness(150%);
   width: 40px;
   transform:translateY(0px);
-  animation: float 3s ease-in-out infinite;
 }
-@keyframes float {
-  0% {
-    transform: translateY(0px)
-  }
-  50% {
-    transform: translateY(-5px)
-  }
-  100% {
-    transform: translateY(0px)
-  }
+.linkedin:hover{
+  	scale:1.5;
+    transition: 1s;
 }
 </style>
